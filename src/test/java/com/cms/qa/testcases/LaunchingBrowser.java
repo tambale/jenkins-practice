841f8d2d3658a29ffbe17f45e4cfe30d2612a74b
saiteja <saiteja2021@gmail.com>
2021-05-17 01:18:52 +0530

    browser version chanhged

--- src/test/java/com/cms/qa/testcases/LaunchingBrowser.java
+++ src/test/java/com/cms/qa/testcases/LaunchingBrowser.java
@@ -10,6 +10,8 @@ public class LaunchingBrowser {
 
 	public static void m1() {
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/java/chromedriver");
+//		System.setProperty("webdriver.chrome.driver", "/home/indoa/Downloads/workspace/testingteamselenium/src/test/java/chromedriver");
+
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://cms.qa.vigocare.com/login?redirect=%2Fcases%2Findex%2Fcaseinfo%2F609e778ee7dc6f002b7d8846");
 		driver.quit();
