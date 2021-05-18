1ae80ed3edb3c999fe3bd8c9d11f092074f349bb
saiteja <saiteja2021@gmail.com>
2021-05-18 17:51:55 +0530

    chromeoptions

--- src/test/java/com/cms/qa/testcases/LaunchingBrowser.java
+++ src/test/java/com/cms/qa/testcases/LaunchingBrowser.java
@@ -10,14 +10,14 @@ public class LaunchingBrowser {
 	public static void m1() {
 		//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/java/chromedriver");
 		System.setProperty("webdriver.chrome.driver", "/home/indoa/Downloads/workspace/testingteamselenium/src/test/java/chromedriver");
-		ChromeOptions options = new ChromeOptions();
+		/*ChromeOptions options = new ChromeOptions();
 		options.addArguments("start-maximized"); // open Browser in maximized mode
 		options.addArguments("disable-infobars"); // disabling infobars
 		options.addArguments("--disable-extensions"); // disabling extensions
 		options.addArguments("--disable-gpu"); // applicable to windows os only
 		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-		options.addArguments("--no-sandbox"); // Bypass OS security model
-		WebDriver driver = new ChromeDriver(options);
+		options.addArguments("--no-sandbox"); // Bypass OS security model*/
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://cms.qa.vigocare.com/login?redirect=%2Fcases%2Findex%2Fcaseinfo%2F609e778ee7dc6f002b7d8846");
 		driver.quit();
 
