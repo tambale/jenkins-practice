f4ebf2c1567e646b8392a08307c36a03acb1e19d
saiteja <saiteja2021@gmail.com>
2021-05-18 16:48:53 +0530

    only one test case

--- src/test/java/com/cms/qa/testcases/LaunchingBrowser.java
+++ src/test/java/com/cms/qa/testcases/LaunchingBrowser.java
@@ -7,9 +7,9 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LaunchingBrowser {
-	@BeforeMethod
+	//@BeforeMethod
 
-	public static void m1() {
+	/*public static void m1() {
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/java/chromedriver");
 //		System.setProperty("webdriver.chrome.driver", "/home/indoa/Downloads/workspace/testingteamselenium/src/test/java/chromedriver");
 		ChromeOptions options = new ChromeOptions();
@@ -20,13 +20,14 @@ public class LaunchingBrowser {
 		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 		options.addArguments("--no-sandbox"); // Bypass OS security model
 		WebDriver driver = new ChromeDriver(options);
-		driver.get("https://cms.qa.vigocare.com/login?redirect=%2Fcases%2Findex%2Fcaseinfo%2F609e778ee7dc6f002b7d8846");
-		driver.quit();
+		//driver.get("https://cms.qa.vigocare.com/login?redirect=%2Fcases%2Findex%2Fcaseinfo%2F609e778ee7dc6f002b7d8846");
+		//driver.quit();
 
-	}
+	}*/
 
 	@Test
 	public static void m2() {
 		System.out.println(" i am in m2 method");
+
 	}
 }
