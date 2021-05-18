5790ddb3c4da7d9d79ce04af1e5188e577186ffe
saiteja <saiteja2021@gmail.com>
2021-05-18 17:03:39 +0530

    changed path

--- src/test/java/com/cms/qa/testcases/LaunchingBrowser.java
+++ src/test/java/com/cms/qa/testcases/LaunchingBrowser.java
@@ -7,11 +7,11 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LaunchingBrowser {
-	//@BeforeMethod
+	@Test
 
-	/*public static void m1() {
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/java/chromedriver");
-//		System.setProperty("webdriver.chrome.driver", "/home/indoa/Downloads/workspace/testingteamselenium/src/test/java/chromedriver");
+	public static void m1() {
+		//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/java/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/home/indoa/Downloads/workspace/testingteamselenium/src/test/java/chromedriver");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("start-maximized"); // open Browser in maximized mode
 		options.addArguments("disable-infobars"); // disabling infobars
@@ -20,10 +20,10 @@ public class LaunchingBrowser {
 		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 		options.addArguments("--no-sandbox"); // Bypass OS security model
 		WebDriver driver = new ChromeDriver(options);
-		//driver.get("https://cms.qa.vigocare.com/login?redirect=%2Fcases%2Findex%2Fcaseinfo%2F609e778ee7dc6f002b7d8846");
-		//driver.quit();
+		driver.get("https://cms.qa.vigocare.com/login?redirect=%2Fcases%2Findex%2Fcaseinfo%2F609e778ee7dc6f002b7d8846");
+		driver.quit();
 
-	}*/
+	}
 
 	@Test
 	public static void m2() {
