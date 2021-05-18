6e30adb217470a843e11a47047cf9d767f1e6bed
saiteja <saiteja2021@gmail.com>
2021-05-18 18:06:43 +0530

    ll

--- src/test/java/com/cms/qa/testcases/LaunchingBrowser.java
+++ src/test/java/com/cms/qa/testcases/LaunchingBrowser.java
@@ -17,7 +17,7 @@ public class LaunchingBrowser {
 		options.addArguments("--disable-gpu"); // applicable to windows os only
 		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 		options.addArguments("--no-sandbox"); // Bypass OS security model*/
-		//WebDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		//driver.get("https://cms.qa.vigocare.com/login?redirect=%2Fcases%2Findex%2Fcaseinfo%2F609e778ee7dc6f002b7d8846");
 		//driver.quit();
 
