7e6bcd0e21edf92d0e149ef9ebaf39e67e932ddd
saiteja <saiteja2021@gmail.com>
2021-05-20 00:52:10 +0530

    geckol

--- src/test/java/com/cms/qa/testcases/LaunchingBrowser.java
+++ src/test/java/com/cms/qa/testcases/LaunchingBrowser.java
@@ -3,6 +3,7 @@ package com.cms.qa.testcases;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -12,16 +13,9 @@ public class LaunchingBrowser {
 	public static void m1() throws InterruptedException {
 		
 		
-		System.setProperty("webdriver.chrome.driver", "/home/indoa/Downloads/geckodriver");
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-		options.addArguments("start-maximized"); // open Browser in maximized mode
-		options.addArguments("disable-infobars"); // disabling infobars
-		options.addArguments("--disable-extensions"); // disabling extensions
-		options.addArguments("--disable-gpu");
-		options.addArguments("--disable-dev-shm-usage");// applicable to windows os only
-		options.addArguments("--no-sandbox"); // Bypass OS security model
-		WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.gecko.driver", "/home/indoa/Downloads/geckodriver");
+		
+		WebDriver driver = new FirefoxDriver();
 		driver.get("https://cms.qa.vigocare.com/login?redirect=%2Fcases%2Findex%2Fcaseinfo%2F609e778ee7dc6f002b7d8846");
 		System.out.println("*********************************");
 		System.out.println(driver.getTitle());
