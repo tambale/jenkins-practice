01952c57b0a4f818bc1c03d1483d3515fac8fb7f
saiteja <saiteja2021@gmail.com>
2021-05-20 00:23:37 +0530

    oooo

--- src/test/java/com/cms/qa/testcases/LaunchingBrowser.java
+++ src/test/java/com/cms/qa/testcases/LaunchingBrowser.java
@@ -12,7 +12,7 @@ public class LaunchingBrowser {
 	public static void m1() throws InterruptedException {
 		
 		
-		System.setProperty("webdriver.chrome.driver", "home/indoa/Downloads/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/home/indoa/Downloads/chromedriver");
 		//System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "\\src\\test\\java\\chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
@@ -22,7 +22,7 @@ public class LaunchingBrowser {
 		options.addArguments("--disable-gpu");
 		options.addArguments("--disable-dev-shm-usage");// applicable to windows os only
 		options.addArguments("--no-sandbox"); // Bypass OS security model
-		WebDriver driver = new ChromeDriver(options);
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://cms.qa.vigocare.com/login?redirect=%2Fcases%2Findex%2Fcaseinfo%2F609e778ee7dc6f002b7d8846");
 		System.out.println("*********************************");
 		System.out.println(driver.getTitle());
