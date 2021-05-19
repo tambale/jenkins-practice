b09cfcc9e13c9d63e13476e72c2c32f37f5644b5
saiteja <saiteja2021@gmail.com>
2021-05-20 00:21:43 +0530

    lmmmmm

--- src/test/java/com/cms/qa/testcases/LaunchingBrowser.java
+++ src/test/java/com/cms/qa/testcases/LaunchingBrowser.java
@@ -12,7 +12,7 @@ public class LaunchingBrowser {
 	public static void m1() throws InterruptedException {
 		
 		
-		System.setProperty("webdriver.chrome.driver", "/home/indoa/Downloads/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "home/indoa/Downloads/chromedriver");
 		//System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "\\src\\test\\java\\chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
