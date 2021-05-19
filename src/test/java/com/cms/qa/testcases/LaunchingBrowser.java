f625b338331763c8768fbc2f6c7464c9ca82b8e0
saiteja <saiteja2021@gmail.com>
2021-05-19 17:49:47 +0530

    dev shm

--- src/test/java/com/cms/qa/testcases/LaunchingBrowser.java
+++ src/test/java/com/cms/qa/testcases/LaunchingBrowser.java
@@ -19,7 +19,8 @@ public class LaunchingBrowser {
 		options.addArguments("start-maximized"); // open Browser in maximized mode
 		options.addArguments("disable-infobars"); // disabling infobars
 		options.addArguments("--disable-extensions"); // disabling extensions
-		options.addArguments("--disable-gpu"); // applicable to windows os only
+		options.addArguments("--disable-gpu");
+		options.addArguments("--disable-dev-shm-usage");// applicable to windows os only
 		options.addArguments("--no-sandbox"); // Bypass OS security model
 		WebDriver driver = new ChromeDriver(options);
 		driver.get("https://cms.qa.vigocare.com/login?redirect=%2Fcases%2Findex%2Fcaseinfo%2F609e778ee7dc6f002b7d8846");
