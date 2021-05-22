3a06184b56d0915bda944d3645dbf94754f51353
BheemeshCirpe <bheemeshtesting007@gmail.com>
2021-05-22 22:09:52 +0530

    Test8

---
+++ Batchfile.bat
@@ -0,0 +1,9 @@
+set projectLocation=System.getProperty("user.dir")+"src/test/java/"
+ 
+cd %projectLocation%
+ 
+set classpath=%projectLocation%\bin;%projectLocation%\lib\*
+ 
+java org.testng.TestNG %projectLocation%\testng.xml
+ 
+pause
